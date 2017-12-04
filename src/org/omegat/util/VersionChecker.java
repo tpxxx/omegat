f8f06044a134faf33c8f773248f742f49066229f
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-12-05 01:08:58 +0900

    Host latest version file in Jenkins

--- src/org/omegat/util/VersionChecker.java
+++ src/org/omegat/util/VersionChecker.java
@@ -41,15 +41,15 @@ public final class VersionChecker {
 
     private static final Logger LOGGER = Logger.getLogger(VersionChecker.class.getName());
 
-    // SourceForge uses strong encryption that can't be used with standard JRE restrictions:
+    // We would prefer to host this on SourceForge, but when HTTPS is enabled SourceForge
+    // uses strong encryption that can't be used with standard JRE restrictions:
     // https://sourceforge.net/p/forge/site-support/14321/
     // Prior to Java 8u151 you had to install the JCE Unlimited Strength Jurisdiction Policy Files:
     // http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
-    // As of Java 8u151 you can set `crypto.policy=unlimited` in java.security.
-    // This is enabled in OmegaT-bundled JREs.
-    // TODO: Come up with a better solution
-    private static final String VERSION_FILE = "https://omegat.sourceforge.io/Version-"
-            + (OStrings.IS_BETA ? "latest" : "standard") + ".properties";
+    // As of Java 8u151 you can set `crypto.policy=unlimited` in java.security, however this
+    // is still too cumbersome for general use.
+    private static final String VERSION_FILE = "https://omegat.ci.cloudbees.com/job/omegat-publish-release-version-"
+            + (OStrings.IS_BETA ? "latest" : "standard") + "/lastSuccessfulBuild/artifact/Version.properties";
 
     private static final int FETCH_INTERVAL = 60 * 5 * 1000;
 
